bdae4344bc882ed77c56c76e522cd843c437a498
Katherine <katherinepmai@gmail.com>
2024-10-14 17:46:38 -0500

    added solution to 1.9

---
+++ python-01/part-01/09_utterances/utterances.py
@@ -0,0 +1,14 @@
+'''
+Here is a program which should ask for three utterances and print them out, like so:
+
+Sample output
+The 1st part: hickory
+The 2nd part: dickory
+The 3rd part: dock
+hickory-dickory-dock!
+'''
+
+part1 = input("The 1st part: ")
+part2 = input("The 2st part: ")
+part3 = input("The 3rd part: ")
+print(part1 + "-" + part2 + "-" + part3 + "!")
